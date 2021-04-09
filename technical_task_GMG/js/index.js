3cb848ae753d1096eae9867d7e11aa8d1f63aac4
Krawdem <42835239+Krawdem@users.noreply.github.com>
2021-04-09 17:05:33 +0300

    Add files via upload

---
+++ technical_task_GMG/js/index.js
@@ -0,0 +1,88 @@
+
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '.splide', {
+        type   : 'loop',
+        perPage: 7,
+        focus  : 'center',
+        trimSpace : true,
+        arrows : false,
+    } ).mount();
+} );
+
+
+
+
+    
+let country_arr = new Array("Afghanistan", "Albania", "Algeria", "American Samoa", "Angola", "Anguilla", "Antartica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Ashmore and Cartier Island", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burma", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Clipperton Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo, Democratic Republic of the", "Congo, Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia", "Cuba", "Cyprus", "Czeck Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Europa Island", "Falkland Islands (Islas Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "French Guiana", "French Polynesia", "French Southern and Antarctic Lands", "Gabon", "Gambia, The", "Gaza Strip", "Georgia", "Germany", "Ghana", "Gibraltar", "Glorioso Islands", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard Island and McDonald Islands", "Holy See (Vatican City)", "Honduras", "Hong Kong", "Howland Island", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Ireland, Northern", "Israel", "Italy", "Jamaica", "Jan Mayen", "Japan", "Jarvis Island", "Jersey", "Johnston Atoll", "Jordan", "Juan de Nova Island", "Kazakhstan", "Kenya", "Kiribati", "Korea, North", "Korea, South", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Man, Isle of", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Midway Islands", "Moldova", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcaim Islands", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romainia", "Russia", "Rwanda", "Saint Helena", "Saint Kitts and Nevis", "Saint Lucia", "Saint Pierre and Miquelon", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Scotland", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and South Sandwich Islands", "Spain", "Spratly Islands", "Sri Lanka", "Sudan", "Suriname", "Svalbard", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Tobago", "Toga", "Tokelau", "Tonga", "Trinidad", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "Uruguay", "USA", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands", "Wales", "Wallis and Futuna", "West Bank", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe");
+let label_arr = ["Prefucture","Emirate","UF","Area Code","Canton","Country","Province"];
+
+
+
+function populateCountries(countryElementId, stateElementId) {
+    // given the id of the <select> tag as function argument, it inserts <option> tags
+    let countryElement = document.getElementById(countryElementId);
+    countryElement.length = 0;
+    countryElement.options[0] = new Option('Choose country');
+    countryElement.selectedIndex = 0;
+    for (let i = 0; i < country_arr.length; i++) {
+        countryElement.options[countryElement.length] = new Option(country_arr[i], country_arr[i]);
+    }
+
+    // Assigned all countries. Now assign event listener for the states.
+
+    if (stateElementId) {
+        countryElement.onchange = function () {
+            populateStates(countryElementId, stateElementId);
+        };
+    }
+}
+populateCountries("country");  
+
+
+let percent = document.querySelectorAll('.slide__percent');
+
+for (let i = 0; i < percent.length; ++i) {
+    if (parseFloat(percent[i].innerHTML) >= 0) {
+        percent[i].style.color = '#05B202';
+    } else {
+        percent[i].style.color = '#DC1B17';
+    };
+};
+
+
+
+ let footerData = document.getElementById("date");
+
+ let newData = (" © " + new Date().getFullYear() + " BrandName. All rights reserved");
+
+ footerData.innerHTML = newData;
+
+
+
+
+
+const dropdown = document.getElementById('dropdown');
+const btn = document.getElementById('group__one');
+const btnP = document.getElementById('group__two');
+const btnR = document.getElementById('group__three');
+const btnE = document.getElementById('group__four');
+
+btn.addEventListener('click', () => {
+    dropdownOne.classList.toggle('hide');
+ } );
+btnP.addEventListener('click', () => {
+    dropdownTwo.classList.toggle('hide');
+ } );
+btnR.addEventListener('click', () => {
+    dropdownThree.classList.toggle('hide');
+ } );
+btnE.addEventListener('click', () => {
+    dropdownFour.classList.toggle('hide');
+ } );
+
+
+ let button = document.querySelector('#btn__reg');
+
+ button.onclick = function() {
+     alert('Registration complete!');
+ }
